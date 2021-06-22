dd982082bd5e8b620ca3166edd60d7f4ed3be327
Farrel Aryo Wahyudi <asakurasoraijn@gmail.com>
2021-06-22 19:52:26 +0700

    Add files via upload

---
+++ Pengenalan Python.py
@@ -0,0 +1,10 @@
+'''
+Author		: Farrel Aryo Wahyudi | Asakura Sora
+Copyright	: FANA Indonesia
+License		: MIT License
+Email		: fanaofc@gmail.com | asakurasoraijn@gmail.com
+Whatsapp	: (+62)899-0396-514
+'''
+
+# Mencetak kalimat お早う！Fayyara Wahyudi!
+print('お早う！Fayyara Wahyudi!')
